f26da417e76f597f5cd4f572e3a93eb8dfd88e5f
GolDRockX <mohsinashfaq136@gmail.com>
2026-07-31 14:08:00 +0500

    Add vercel.json with function config 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState, useRef } from 'react';
 import { io } from 'socket.io-client';
 import './App.css';
 
-const socket = io('https://chat-app-xyz.vercel.app', {
+const socket = io('https://chat-app-navy-rho-72.vercel.app', {
   path: '/api/socket/socket.io',
   transports: ['websocket']
 });
